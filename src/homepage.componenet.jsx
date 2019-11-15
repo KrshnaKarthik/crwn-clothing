3e7f77e0b31c5a5e3ad08c62cc476b056514e6c6
Karthik <karthik.maranahalli@tesco.com>
2019-11-15 16:36:27 +0530

    Crown clothing home page first commit

---
+++ src/homepage.componenet.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () =>(
+    <div className="homepage">
+    <div className="directory-menu">
+    
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 title='title'>Hats </h1>
+    <span className="subtitle">Shop Now</span>
+    </div>
+    </div>
+
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 title='title'>shoes </h1>
+    <span className="subtitle">Shop Now</span>
+    </div>
+    </div>
+
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 title='title'>Jackets </h1>
+    <span className="subtitle">Shop Now</span>
+    </div>
+    </div>
+
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 title='title'>Mens </h1>
+    <span className="subtitle">Shop Now</span>
+    </div>
+    </div>
+
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 title='title'>Womens </h1>
+    <span className="subtitle">Shop Now</span>
+    </div>
+    </div>
+
+
+
+
+    </div> 
+    </div>
+)
+
+export default HomePage;
