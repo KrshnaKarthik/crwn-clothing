9b02bf0f2e006b0c5273bf7b28a6502a0c3ed166
Karthik <karthik.maranahalli@tesco.com>
2019-11-16 23:49:10 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.componenet';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from  './components/header/header.component';
 import {Route} from 'react-router-dom';
 
 
@@ -9,6 +10,7 @@ import {Route} from 'react-router-dom';
 function App() {
   return (
     <div >
+    <Header/>
     <Route exact path='/' component={HomePage}/>
     <Route exact path='/shop' component={ShopPage}/>
     
