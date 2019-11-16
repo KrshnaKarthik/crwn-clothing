df05b4ff3b5c6455357cf0f3f7b262b14c5b578c
Karthik <karthik.maranahalli@tesco.com>
2019-11-16 11:39:59 +0530

    router setup as HOC for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.componenet';
+import {Route} from 'react-router-dom';
+
+const HatsPage= () =>(
+  <div>
+  <h1>HatsPage!</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-    <HomePage />
+    <Route exact path='/' component={HomePage}/>
+    <Route exact path='/hats' component={HatsPage}/>
+    
     </div>
   );
 }
